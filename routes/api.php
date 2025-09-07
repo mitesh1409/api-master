ab6d0a596b1a4fa8d7baa02469d44de05ff33498
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2025-09-07 22:52:35 +0530

    Change login API method to POST

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get("/login", [AuthController::class, 'login']);
+Route::post("/login", [AuthController::class, 'login']);
