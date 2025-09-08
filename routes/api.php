6400c42cd4cd790cebf4b399c3a77f4f735878c7
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2025-09-08 14:25:07 +0530

    App api endpoint to get all the tickets

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,11 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Models\Ticket;
 use Illuminate\Support\Facades\Route;
 
 Route::post("/login", [AuthController::class, 'login']);
+
+Route::get('tickets', function () {
+    return Ticket::all();
+});
